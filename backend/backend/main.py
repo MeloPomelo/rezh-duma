490d1f676d6cd295754aedad33f1b079058185bd
ARTSOFTE\sdorokhov <seregapax@gmail.com>
2022-06-04 22:03:50 +0500

    env

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.core.config import DEBUG, PROJECT_NAME, VERSION
 from backend.db.events import start_app_handler, stop_app_handler
 import backend.api.routes as routes
 
 app = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 api_routes = ((routes.applications_router, "applications"),
               (routes.faq_router, "faqs"))
