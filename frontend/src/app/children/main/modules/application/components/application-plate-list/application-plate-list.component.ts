8703d906073e17d01d5b967aa223e2fe254b68f7
zuppi <seregapax@gmail.com>
2022-06-14 23:48:03 +0500

    add tags

--- frontend/src/app/children/main/modules/application/components/application-plate-list/application-plate-list.component.ts
+++ frontend/src/app/children/main/modules/application/components/application-plate-list/application-plate-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 import { TuiDestroyService } from '@taiga-ui/cdk';
-import { ApplicationModel, ApplicationPlateViewModel, IListOfModels } from '../../../../../../models';
+import { ApplicationModel, ApplicationPlateViewModel, IApplicationsRequestModel, IListOfModels } from '../../../../../../models';
 import { ApplicationsRequestsService } from '../../services/applications.request-service';
 
 @Component({
@@ -17,10 +17,17 @@ export class ApplicationPlateListComponent {
     public modelSubject$: BehaviorSubject<ApplicationPlateViewModel[] | null> = new BehaviorSubject<ApplicationPlateViewModel[] | null>(null);
     public hasMore: boolean = false;
 
+    private _tags!: string[];;
+
     constructor(
         private _applicationsRequestsService: ApplicationsRequestsService,
         private _destroyService: TuiDestroyService
     ){
+        this.initModel();
+    }
+
+    public addTags(tags: string[]): void {
+        this._tags = tags;
         this.initModel();
     }
 
@@ -40,10 +47,12 @@ export class ApplicationPlateListComponent {
     }
 
     private loadData(offset: number, limit: number): Observable<ApplicationPlateViewModel[]> {
-        return this._applicationsRequestsService.getItems({
-            offset,
-            limit
-        }, this._destroyService)
+        const options: IApplicationsRequestModel = { offset, limit };
+        if (this._tags && this._tags.length) {
+            options.tags = this._tags;
+        }
+
+        return this._applicationsRequestsService.getItems(options, this._destroyService)
             .pipe(
                 map((model: IListOfModels<ApplicationModel>) => {
                     this.hasMore = model.count + (this.modelSubject$.getValue() ?? []).length < model.total;
